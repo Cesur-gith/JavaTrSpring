034fb8e6d239a7fdcd18299785a78fe557dbf3a7
Jasur <48770149+Cesur-gith@users.noreply.github.com>
2022-03-20 22:23:46 +0100

    Add files via upload

---
+++ day21Array/ArrayQ01AssignName.java
@@ -0,0 +1,18 @@
+package day21Array;
+
+import java.util.Arrays;
+
+public class ArrayQ01AssignName {
+
+    public static void main(String[] args) {
+
+        String myArray[] = {"Ali", "Ayse"};
+
+        myArray[0] = "Veli";
+        myArray[1] = "Fatma";
+
+        System.out.println(Arrays.toString(myArray));
+
+
+    }
+}
---
+++ day21Array/ArraysSumQ02.java
@@ -0,0 +1,15 @@
+package day21Array;
+
+public class ArraysSumQ02 {
+
+
+    public static void main(String[] args) {
+
+        int myArray [] = {1,2,3};
+
+//        int myNewArray[] = new
+
+
+    }
+
+}
